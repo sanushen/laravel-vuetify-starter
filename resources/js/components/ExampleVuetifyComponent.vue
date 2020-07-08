6dceff6095e3e7049f55fbb43cfb4ecc0c50b783
Sanushen Govender <sanushen@gmail.com>
2020-07-08 12:41:52 +0200

    Inital commit

---
+++ resources/js/components/ExampleVuetifyComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app>
+        <v-card
+            class="mx-auto"
+            max-width="900"
+        >
+            <v-card-text>
+                <div>Hello</div>
+                <p class="display-1 text--primary">
+                    This is a Vuetify card component
+                </p>
+                <p>Please see the example component</p>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn
+                    text
+                    color="deep-purple accent-4"
+                    href="login"
+                >
+                    Login
+                </v-btn>
+                <v-btn
+                    text
+                    color="deep-purple accent-4"
+                    href="register"
+                >
+                    Register
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ webpack.mix.js
@@ -0,0 +1,27 @@
+const mix = require('laravel-mix');
+
+var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+var webpackConfig = {
+    plugins: [
+        new CaseSensitivePathsPlugin(),
+        new VuetifyLoaderPlugin()
+    ]
+    // other webpack config ...
+}
+
+mix.webpackConfig(webpackConfig);
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
